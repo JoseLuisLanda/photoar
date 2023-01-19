28b4ab48ae984b06041df1bdcc8f28d12cd8ded6
JoseLuisLanda <viajero13279@innovaccion.mx>
2023-01-19 15:40:34 -0600

    luli tuzo

--- src/app/shared/arheader/arheader.component.ts
+++ src/app/shared/arheader/arheader.component.ts
@@ -9,6 +9,7 @@ export class ARHeaderComponent implements OnInit, OnChanges {
   @Output() changeModel: EventEmitter<string> = new EventEmitter<string>();
   nombreModelo: Elemento[] = [
 //models tuzo
+{name:"Luli tuzo",url:"../../../assets/models/lulifull.glb",},
 {name:"Luli",url:"../../../assets/models/luliplane.glb",},
 {name:"Duende",url:"../../../assets/models/duende.glb",},
     {name:"Gnomo",url:"../../../assets/models/nomocut.glb",},//      36

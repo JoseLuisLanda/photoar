7697ee9259696854cf59b4000402014fa40e5879
JoseLuisLanda <viajero13279@innovaccion.mx>
2023-04-25 12:18:10 -0600

    formatting code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,24 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { ElementId, Evento, Grupo, UserModel } from '../collections/element';
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { FotosService } from 'src/app/shared/services/fotos.service'
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { FotosService } from 'src/app/shared/services/fotos.service';
 import { map, tap } from 'rxjs/operators';
 import { Router, ActivatedRoute } from '@angular/router';
-import html2canvas from "html2canvas"; 
+import html2canvas from 'html2canvas';
 import { Observable, Subject, merge, of } from 'rxjs';
 import { AuthService } from '../shared/services/auth.service';
-import { speechrecognition } from '../shared/services/speechrecognition.service'
+import { speechrecognition } from '../shared/services/speechrecognition.service';
 import { SpeechRecognizerService } from '../shared/services/speech-recognizer.service';
 import { SpeechError } from '../model/speech-error';
 import { SpeechEvent } from '../model/speech-event';
@@ -18,7 +29,7 @@ import { SpeechNotification } from '../model/speech-notification';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [speechrecognition]
+  providers: [speechrecognition],
 })
 export class HomeComponent implements OnInit, AfterViewInit, OnChanges {
   totalTranscript?: string;
@@ -30,270 +41,276 @@ export class HomeComponent implements OnInit, AfterViewInit, OnChanges {
   currentLanguage: string = defaultLanguage;
 
   @Output() item: EventEmitter<ElementId> = new EventEmitter<ElementId>();
-  switchTemp: boolean  = false;
+  switchTemp: boolean = false;
   title = 'Visualiz-AR';
-  itemAR:ElementId={uid:"sky",name:"../../../assets/models/Astronaut.glb"};
-  elements: ElementId[]=[];
-  lugares: ElementId[]=[{uid: "2", name: "Foto", description:"foto"}];
-  codes: ElementId = {uid:"",codes:["general"]};
+  itemAR: ElementId = {
+    uid: 'sky',
+    name: '../../../assets/models/Astronaut.glb',
+  };
+  elements: ElementId[] = [];
+  lugares: ElementId[] = [{ uid: '2', name: 'Foto', description: 'foto' }];
+  codes: ElementId = { uid: '', codes: ['general'] };
   subElements?: ElementId[];
   myPhoto?: ElementId;
-  search:boolean =  true;
-  elementNumber = "";
-  textError = "";
-  caller="Lugares";
-  place = "lugares";
-  folderToSearch = "foto";
+  search: boolean = true;
+  elementNumber = '';
+  textError = '';
+  caller = 'Lugares';
+  place = 'lugares';
+  folderToSearch = 'foto';
   showCodeDiv = true;
-  users = [{},{},{},{}];
-  location:string = "general";
-  folder:string = "lugares";
+  users = [{}, {}, {}, {}];
+  location: string = 'general';
+  folder: string = 'lugares';
   uploadimage: boolean = true;
-  userItem : UserModel = new UserModel();
-  currentItem: any ={};
+  userItem: UserModel = new UserModel();
+  currentItem: any = {};
   isForm = false;
   emailConfirmed = false;
   @ViewChild('screen') screen: ElementRef;
   @ViewChild('canvas') canvas: ElementRef;
   @ViewChild('downloadLink') downloadLink: ElementRef;
   public user$: Observable<any> = this.authSvc.afAuth.user;
-  constructor( private speechRecognizer: SpeechRecognizerService, public serviceRecognition : speechrecognition, private router: Router, private authSvc: AuthService, private activeRoute: ActivatedRoute, private fotosService: FotosService) {
-    console.log("getting places");
-    this.fotosService.getCollection("lugares", 50,"","","codes","general").subscribe((data) => {
-      if(data !== undefined)
-      this.lugares =   data.filter(obj => {
-        return obj.normalizedName != "general"
+  constructor(
+    private speechRecognizer: SpeechRecognizerService,
+    public serviceRecognition: speechrecognition,
+    private router: Router,
+    private authSvc: AuthService,
+    private activeRoute: ActivatedRoute,
+    private fotosService: FotosService
+  ) {
+    console.log('getting places');
+    this.fotosService
+      .getCollection('lugares', 50, '', '', 'codes', 'general')
+      .subscribe((data) => {
+        if (data !== undefined)
+          this.lugares = data.filter((obj) => {
+            return obj.normalizedName != 'general';
+          });
+        this.codes = data.find((obj) => {
+          return obj.normalizedName == 'general';
+        });
+
+        //console.log("GETTING chat messages: "+JSON.stringify(this.users));
       });
-      this.codes = data.find(obj => {
-        return obj.normalizedName == "general"
-      });
-      
-      //console.log("GETTING chat messages: "+JSON.stringify(this.users));
-    });
-    
+
     //this.serviceRecognition.init()
-   }
-  ngAfterViewInit(){
+  }
+  ngAfterViewInit() {
     //console.log(this.screen)
     this.emailConfirmed = this.authSvc.isLoggedIn;
- }
- startService(){
-  this.serviceRecognition.start()
-}
-
-stopService(){
-  this.serviceRecognition.stop()
-}
-startSpeech(){
-  this.listening$ = merge(
-    this.speechRecognizer.onStart(),
-    this.speechRecognizer.onEnd()
-  ).pipe(map((notification) => notification.event === SpeechEvent.Start));
-
-  this.serviceRecognition.speechNow("llamandote desde home");
-}
-start(): void {
-  if (this.speechRecognizer.isListening) {
-    this.stop();
-    return;
+  }
+  startService() {
+    this.serviceRecognition.start();
   }
 
-  this.defaultError$.next(undefined);
-  this.speechRecognizer.start();
-}
+  stopService() {
+    this.serviceRecognition.stop();
+  }
+  startSpeech() {
+    this.listening$ = merge(
+      this.speechRecognizer.onStart(),
+      this.speechRecognizer.onEnd()
+    ).pipe(map((notification) => notification.event === SpeechEvent.Start));
 
-stop(): void {
-  this.speechRecognizer.stop();
-}
+    this.serviceRecognition.speechNow('llamandote desde home');
+  }
+  start(): void {
+    if (this.speechRecognizer.isListening) {
+      this.stop();
+      return;
+    }
+
+    this.defaultError$.next(undefined);
+    this.speechRecognizer.start();
+  }
+
+  stop(): void {
+    this.speechRecognizer.stop();
+  }
   ngOnInit(): void {
-   
     this.emailConfirmed = this.authSvc.isLoggedIn;
-    this.activeRoute.queryParams
-    .subscribe(params => {
+    this.activeRoute.queryParams.subscribe((params) => {
       //console.log(params); // { orderby: "location" }
       this.elements = [];
-      if(params.caller !== undefined){
-        this.location = params.code !== undefined ? params.code : "general";
+      if (params.caller !== undefined) {
+        this.location = params.code !== undefined ? params.code : 'general';
         this.getElements(params.type);
-      }else if(params.type !== undefined &&  params.code !== undefined)
-      {
+      } else if (params.type !== undefined && params.code !== undefined) {
         this.location = params.code;
         this.folder = params.type;
         //console.log(this.location); // location
-        this.fotosService.getCollection(this.folder, 50,"","","codes",this.location).subscribe((data) => {
-          if(data !== undefined && data.length > 0){
-            this.elements =   data as ElementId[];
-            this.itemAR = this.elements[0];
-        //this.itemAR.type = "place";
-        this.switchTemp = true;
-          //console.log("img elements: "+JSON.stringify(this.elements))
-          }
-          
-          this.lugares =   data.filter(obj => {
-            return obj.normalizedName != "general"
-          });
-          this.codes = data.find(obj => {
-            return obj.normalizedName == "general"
-          });
+        this.fotosService
+          .getCollection(this.folder, 50, '', '', 'codes', this.location)
+          .subscribe((data) => {
+            if (data !== undefined && data.length > 0) {
+              this.elements = data as ElementId[];
+              this.itemAR = this.elements[0];
+              //this.itemAR.type = "place";
+              this.switchTemp = true;
+              //console.log("img elements: "+JSON.stringify(this.elements))
+            }
 
-          //console.log("GETTING chat messages: "+JSON.stringify(this.users));
-        });
-      }
-      if(params.code !== undefined){
-        this.fotosService.getCollection("lugares", 50,"","","codes","general").subscribe((data) => {
-          if(data !== undefined)
-          this.lugares =   data.filter(obj => {
-            return obj.normalizedName != "general"
+            this.lugares = data.filter((obj) => {
+              return obj.normalizedName != 'general';
+            });
+            this.codes = data.find((obj) => {
+              return obj.normalizedName == 'general';
+            });
+
+            //console.log("GETTING chat messages: "+JSON.stringify(this.users));
           });
-          this.codes = data.find(obj => {
-            return obj.normalizedName == "general"
-          });
-          
-          //console.log("GETTING chat messages: "+JSON.stringify(this.users));
-        });
       }
-     
-       
-      
-      
-    }
-  );
-    
+      if (params.code !== undefined) {
+        this.fotosService
+          .getCollection('lugares', 50, '', '', 'codes', 'general')
+          .subscribe((data) => {
+            if (data !== undefined)
+              this.lugares = data.filter((obj) => {
+                return obj.normalizedName != 'general';
+              });
+            this.codes = data.find((obj) => {
+              return obj.normalizedName == 'general';
+            });
+
+            //console.log("GETTING chat messages: "+JSON.stringify(this.users));
+          });
+      }
+    });
   }
 
-
-  gotTo(page: string){
-    this.router.navigateByUrl('/'+page);
+  gotTo(page: string) {
+    this.router.navigateByUrl('/' + page);
   }
-  ngOnChanges(){
+  ngOnChanges() {
     this.emailConfirmed = this.authSvc.isLoggedIn;
   }
-  getElements(type: string){
-   this.textError = "";
-   this.fotosService.getCollection(type, 50,"","","codes",this.location).subscribe((data) => {
-    this.elements = [];
-    if(data !== undefined && data.length > 0)
-    {
-      switch (type){
-          case "lugares":
-            this.lugares =   data.filter(obj => {
-              return obj.normalizedName != "general"
-            });
-            this.codes = data.find(obj => {
-              return obj.normalizedName == "general"
-            });
-            (<HTMLInputElement> document.getElementById("collapseOne")).setAttribute("class","show");
-            break;
+  getElements(type: string) {
+    this.textError = '';
+    this.fotosService
+      .getCollection(type, 50, '', '', 'codes', this.location)
+      .subscribe((data) => {
+        this.elements = [];
+        if (data !== undefined && data.length > 0) {
+          switch (type) {
+            case 'lugares':
+              this.lugares = data.filter((obj) => {
+                return obj.normalizedName != 'general';
+              });
+              this.codes = data.find((obj) => {
+                return obj.normalizedName == 'general';
+              });
+              (<HTMLInputElement>(
+                document.getElementById('collapseOne')
+              )).setAttribute('class', 'show');
+              break;
             default:
-            this.elements =   data.filter(obj => {
-              return obj.name != "default"
-            });
-            
-            if(this.elements!.length<1)
-             {
-              this.textError = "no existe contenido para este lugar";
-              
-             }
-            break;
-      }
-     
-    }else{
-      (<HTMLInputElement> document.getElementById("collapseOne")).setAttribute("class","show");
-      
-      this.textError = "no existe contenido para este lugar";
-    }
-    
-    //console.log("GETTING chat messages: "+JSON.stringify(this.users));
-  });
-  
+              this.elements = data.filter((obj) => {
+                return obj.name != 'default';
+              });
 
-  }
-  getARElement(){
-    this.textError = "";
-    //this.getElements(this.place.toLowerCase());
-    //console.log("foldersearch: "+this.folderToSearch+" elementnumber"+this.elementNumber);
-    if(this.elementNumber !== ""){
-      console.log("sending request: ");
-      this.fotosService.getCollection(this.folderToSearch, 50,"description",this.elementNumber.toLowerCase()).subscribe((data) => {
-        //console.log("getting data: ");
-        if(data !== undefined)
-        this.elements =   data as ElementId[];
-        if(this.elements!.length <1){
-          this.textError = "No hay contenido para este código";
-        }else{
-          
-          this.itemAR = this.elements[0];
-        this.itemAR.type = "place";
-        this.switchTemp = true;
-          //console.log("img elements: "+JSON.stringify(this.elements))
+              if (this.elements!.length < 1) {
+                this.textError = 'no existe contenido para este lugar';
+              }
+              break;
+          }
+        } else {
+          (<HTMLInputElement>(
+            document.getElementById('collapseOne')
+          )).setAttribute('class', 'show');
+
+          this.textError = 'no existe contenido para este lugar';
         }
+
         //console.log("GETTING chat messages: "+JSON.stringify(this.users));
       });
-    }else{
-      this.textError = "ingresa un código VÁLIDO";
-    }
-   
-    
   }
-  switchToPlace(event: any){
+  getARElement() {
+    this.textError = '';
+    //this.getElements(this.place.toLowerCase());
+    //console.log("foldersearch: "+this.folderToSearch+" elementnumber"+this.elementNumber);
+    if (this.elementNumber !== '') {
+      console.log('sending request: ');
+      this.fotosService
+        .getCollection(
+          this.folderToSearch,
+          50,
+          'description',
+          this.elementNumber.toLowerCase()
+        )
+        .subscribe((data) => {
+          //console.log("getting data: ");
+          if (data !== undefined) this.elements = data as ElementId[];
+          if (this.elements!.length < 1) {
+            this.textError = 'No hay contenido para este código';
+          } else {
+            this.itemAR = this.elements[0];
+            this.itemAR.type = 'place';
+            this.switchTemp = true;
+            //console.log("img elements: "+JSON.stringify(this.elements))
+          }
+          //console.log("GETTING chat messages: "+JSON.stringify(this.users));
+        });
+    } else {
+      this.textError = 'ingresa un código VÁLIDO';
+    }
+  }
+  switchToPlace(event: any) {
     //console.log("otro modelo received: " + JSON.stringify(event));
-    
+
     this.itemAR = event;
-    this.itemAR.type = "photos";
-        this.switchTemp = true;
+    this.itemAR.type = 'photos';
+    this.switchTemp = true;
   }
 
-  receiveModel(modelName: string){
+  receiveModel(modelName: string) {
     //console.log("otro modelo received: " + modelName);
     this.itemAR.name = modelName;
   }
-  switchTemplate(){
+  switchTemplate() {
     this.switchTemp = !this.switchTemp;
   }
-  onSelectChange(event: string){
+  onSelectChange(event: string) {
     //getting elements every onchange places dropdown fires
-    this.textError = "";
-   
-      this.getElements( event.toLowerCase());
+    this.textError = '';
+
+    this.getElements(event.toLowerCase());
   }
-  onSelectBtn(event: string){
+  onSelectBtn(event: string) {
     //getting elements every onchange places dropdown fires
-    this.textError = "";
- 
-      this.getElements( event.toLowerCase());
+    this.textError = '';
+
+    this.getElements(event.toLowerCase());
   }
-  showmyCodeDiv(value:boolean, type:string){
+  showmyCodeDiv(value: boolean, type: string) {
     //console.log("Type ",type);
     this.folderToSearch = type;
     this.showCodeDiv = value;
   }
-  changePlace(place:string){
+  changePlace(place: string) {
     //console.log("receiving: "+place);
     this.location = place;
-    this.getElements("lugares");
-    (<HTMLInputElement> document.getElementById("showingModal")).click();
+    this.getElements('lugares');
+    (<HTMLInputElement>document.getElementById('showingModal')).click();
   }
-  downloadImage(){
-    html2canvas(this.screen.nativeElement).then(canvas => {
+  downloadImage() {
+    html2canvas(this.screen.nativeElement).then((canvas) => {
       this.canvas.nativeElement.src = canvas.toDataURL();
       this.downloadLink.nativeElement.href = canvas.toDataURL('image/png');
       this.downloadLink.nativeElement.download = 'marble-diagram.png';
       this.downloadLink.nativeElement.click();
     });
   }
-  signUp(){
+  signUp() {
     this.uploadimage = false;
-    this.caller = "SingUp";
+    this.caller = 'SingUp';
     this.isForm = true;
     this.currentItem = this.userItem;
-    (<HTMLInputElement> document.getElementById("showingModal")).click();
+    (<HTMLInputElement>document.getElementById('showingModal')).click();
   }
-  setLugares(){
+  setLugares() {
     this.uploadimage = false;
-    this.caller = "Lugares";
+    this.caller = 'Lugares';
     this.isForm = false;
   }
-
 }
-
-

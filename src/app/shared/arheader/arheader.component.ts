58d067f98f23742645fab086702ea534ab1e43f7
JoseLuisLanda <viajero13279@innovaccion.mx>
2023-05-13 17:26:44 -0600

    test menues

--- src/app/shared/arheader/arheader.component.ts
+++ src/app/shared/arheader/arheader.component.ts
@@ -9,8 +9,8 @@ export class ARHeaderComponent implements OnInit, OnChanges {
   @Output() changeModel: EventEmitter<string> = new EventEmitter<string>();
   nombreModelo: Elemento[] = [
     //models tuzo
-    { name: "Comida", url: "../../../assets/models/menu_both.glb", },
-    { name: "bebida", url: "../../../assets/models/menu_oriented.glb", },
+    { name: "Comida", url: "../../../assets/models/menu_chico.glb", },
+    { name: "bebida", url: "../../../assets/models/menu_close.glb", },
     { name: "delorean Cube", url: "../../../assets/models/delorean_cube.glb", },
     { name: "orange Cube", url: "../../../assets/models/orange_cube.glb", },
     { name: "ChaseCube", url: "../../../assets/models/chase_cube.glb", },

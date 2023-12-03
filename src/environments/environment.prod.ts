fce96927692427debffe7111b6963ee854875015
JoseLuisLanda <viajero13279@innovaccion.mx>
2023-12-03 09:03:52 -0600

    actions for publicatulancingo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,22 +2,22 @@ export const environment = {
   production: true,
   serviceURL : "https://us-central1-apisvc.cloudfunctions.net/api",
   firebase: {
-    apiKey: "AIzaSyAEvhG7q00C_Q58kNx2WrB-M3yUVuK6nU8",
-    authDomain: "uptamira.firebaseapp.com",
-    projectId: "uptamira",
-    storageBucket: "uptamira.appspot.com",
-    messagingSenderId: "588176673481",
-    appId: "1:588176673481:web:1714763c5dc49d5ad2db18",
-    measurementId: "G-9RH6MVJY3R"
+    apiKey: "AIzaSyBOXX0Hqml7zoW4pDuuBgVxUYzT1JzaS-o",
+    authDomain: "publicartulancingo.firebaseapp.com",
+    projectId: "publicartulancingo",
+    storageBucket: "publicartulancingo.appspot.com",
+    messagingSenderId: "329096813263",
+    appId: "1:329096813263:web:95e35b6ab0aed05d2175ba",
+    measurementId: "G-YD7BLL11Q9"
   },
   firebaseConfig : {
-    apiKey: "AIzaSyAEvhG7q00C_Q58kNx2WrB-M3yUVuK6nU8",
-    authDomain: "uptamira.firebaseapp.com",
-    projectId: "uptamira",
-    storageBucket: "uptamira.appspot.com",
-    messagingSenderId: "588176673481",
-    appId: "1:588176673481:web:1714763c5dc49d5ad2db18",
-    measurementId: "G-9RH6MVJY3R"
+    apiKey: "AIzaSyBOXX0Hqml7zoW4pDuuBgVxUYzT1JzaS-o",
+  authDomain: "publicartulancingo.firebaseapp.com",
+  projectId: "publicartulancingo",
+  storageBucket: "publicartulancingo.appspot.com",
+  messagingSenderId: "329096813263",
+  appId: "1:329096813263:web:95e35b6ab0aed05d2175ba",
+  measurementId: "G-YD7BLL11Q9"
   },
   prompts : 
     ["quiero","dame","enseñame","dime","proyecta","muestrame","muéstrame", "busca","búscame","buscame","muestra","buscar","mostrar","listar"],
